3d5bc9b8756f8ae8e64d492763a02e06cf288966
mattyeux <fay.matthieu@gmail.com>
2022-10-01 13:46:32 +0200

    draw_pitch OK

--- GAME/FMIA_GAME.py
+++ GAME/FMIA_GAME.py
@@ -1,8 +1,8 @@
 import pygame
+from fmia_pitch import Pitch
 
-
-_WIDTH = 1000
-_HEIGHT = 500
+_WIDTH = 1200
+_HEIGHT = 800
 
 
 # Initialisation
@@ -12,10 +12,18 @@ pygame.init()
 root = pygame.display.set_mode((_WIDTH, _HEIGHT))
 pygame.display.set_caption("Football Manager IA")
 background = pygame.display.get_surface()
+game_pitch = Pitch()
+
+
+def draw_pitch():
+    root.blit(game_pitch.pitch_frame, (0, 0))
+    game_pitch.render()
+
 
 while running:
-    background.fill((126, 200, 80))
+    background.fill((255, 255, 255))
     root.blit(background, (0, 0))
+    draw_pitch()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
---
+++ GAME/fmia_pitch.py
@@ -0,0 +1,52 @@
+import pygame
+
+_PITCH_WIDTH = 1200
+_PITCH_HEIGHT = 800
+_TOUCH_WIDTH = 50
+_CENTRAL_CIRCLE_RADIUS = 91.5
+_LINE_THICKNESS = 3
+_SURFACE_WITDH = 165
+_SURFACE_HEIGHT = 403
+_GOAL_SURFACE_WIDTH = 55
+_GOAL_SURFACE_HEIGHT = _SURFACE_HEIGHT-2*110
+_PENALT_DISTANCE = 110
+_PENALT_SIZE = 5
+_CENTRAL_SIZE = 5
+_GOAL_WIDTH = 15
+_GOAL_HEIGHT = 73.2
+
+
+class Pitch(object):
+
+    def __init__(self):
+
+        self.clock_update = pygame.time.Clock()  # clock to manage time update and FPS
+        self.fall_time = 0  # time to give rythm of the game update (and the falling active piece)
+        self.game_time = 0  # time to store the total time
+        self.pitch_frame = pygame.Surface((_PITCH_WIDTH, _PITCH_HEIGHT))  # main surface
+        self.pitch_frame.fill((126, 200, 80))
+        # self.game.launch_piece()  # launch the first active piece
+        self.highlighted = False  # if the frame is highl
+    #    pygame.draw.rect(self.game_frame, (255, 255, 255), (0, 0, 100, 100), 2)
+
+    def render(self):
+        # dessine les lignes du terrain
+        pygame.draw.rect(self.pitch_frame, (255, 255, 255), (_TOUCH_WIDTH, _TOUCH_WIDTH, _PITCH_WIDTH-2*_TOUCH_WIDTH, _PITCH_HEIGHT-2*_TOUCH_WIDTH), _LINE_THICKNESS)
+        # dessine la line médiane
+        pygame.draw.line(self.pitch_frame, (255, 255, 255), (_PITCH_WIDTH/2, _TOUCH_WIDTH), (_PITCH_WIDTH/2, _PITCH_HEIGHT-_TOUCH_WIDTH), _LINE_THICKNESS)
+        # dessine le rond central
+        pygame.draw.circle(self.pitch_frame, (255, 255, 255), (_PITCH_WIDTH/2-1, _PITCH_HEIGHT/2-1), _CENTRAL_CIRCLE_RADIUS, width=_LINE_THICKNESS)
+        # dessine les surfaces de réparation
+        pygame.draw.rect(self.pitch_frame, (255, 255, 255), (_TOUCH_WIDTH, (_PITCH_HEIGHT-_SURFACE_HEIGHT)/2, _TOUCH_WIDTH+_SURFACE_WITDH, _SURFACE_HEIGHT), _LINE_THICKNESS)
+        pygame.draw.rect(self.pitch_frame, (255, 255, 255), (_PITCH_WIDTH-_TOUCH_WIDTH*2-_SURFACE_WITDH, (_PITCH_HEIGHT-_SURFACE_HEIGHT)/2, _TOUCH_WIDTH+_SURFACE_WITDH, _SURFACE_HEIGHT), _LINE_THICKNESS)
+        # dessine les surfaces du goal
+        pygame.draw.rect(self.pitch_frame, (255, 255, 255), (_TOUCH_WIDTH, (_PITCH_HEIGHT-_GOAL_SURFACE_HEIGHT)/2, _GOAL_SURFACE_WIDTH, _GOAL_SURFACE_HEIGHT), _LINE_THICKNESS)
+        pygame.draw.rect(self.pitch_frame, (255, 255, 255), (_PITCH_WIDTH-_TOUCH_WIDTH-_GOAL_SURFACE_WIDTH, (_PITCH_HEIGHT-_GOAL_SURFACE_HEIGHT)/2, _GOAL_SURFACE_WIDTH, _GOAL_SURFACE_HEIGHT), _LINE_THICKNESS)
+        # dessine le point de pénalty
+        pygame.draw.circle(self.pitch_frame, (255, 255, 255), (_TOUCH_WIDTH+_PENALT_DISTANCE, _PITCH_HEIGHT/2-1), _PENALT_SIZE, width=0)
+        pygame.draw.circle(self.pitch_frame, (255, 255, 255), (_PITCH_WIDTH-_TOUCH_WIDTH-_PENALT_DISTANCE, _PITCH_HEIGHT/2-1), _PENALT_SIZE, width=0)
+        # dessine le point central
+        pygame.draw.circle(self.pitch_frame, (255, 255, 255), (_PITCH_WIDTH/2, _PITCH_HEIGHT/2), _CENTRAL_SIZE, width=0)
+        # dessine les cages
+        pygame.draw.rect(self.pitch_frame, (158, 158, 158), (_TOUCH_WIDTH-_GOAL_WIDTH+2, (_PITCH_HEIGHT-_GOAL_HEIGHT)/2, _GOAL_WIDTH, _GOAL_HEIGHT), 0)
+        pygame.draw.rect(self.pitch_frame, (158, 158, 158), (_PITCH_WIDTH-_TOUCH_WIDTH-2, (_PITCH_HEIGHT-_GOAL_HEIGHT)/2, _GOAL_WIDTH, _GOAL_HEIGHT), 0)
